9ac0780253fdd62e8b101136d385842fdbbdc53e
Altair_Kenway <159497209+MigueAAM@users.noreply.github.com>
2025-06-13 10:46:50 -0500

    Add files via upload

---
+++ Design/menu/menu_function.js
@@ -0,0 +1,6 @@
+/* ====================================================
+nav bar functions
+=======================================================*/
+function navigate(url) {
+    window.location.href = url; // Redirects to the specified page
+}
